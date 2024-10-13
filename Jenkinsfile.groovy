ce109b7298447f8c428c11e9e9ca9926cba2985e
gtello79 <gonzalotello79@gmail.com>
2024-10-13 09:07:15 -0300

    Agregando Jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline{
+    agent any
+
+    stages{
+        stage('Compile Stage'){
+            steps{
+                withMaven('manven_3_9_5'){
+                    sh'mvn clean compile'
+                }
+            }
+        }
+
+        stage('Test Stage'){
+            steps{
+                withMaven('manven_3_9_5'){
+                    sh'mvn test'
+                }
+            }
+        }
+
+        stage('Cucumber Reports'){
+            steps{
+                cucumber buildStatus: 'UNSTABLE', 
+                        fileIncludePattern: '**/target/cucumber-reports/*.json', 
+                        jsonReportDirectory: 'target/cucumber-reports',
+            }
+        }
+    }
+}
