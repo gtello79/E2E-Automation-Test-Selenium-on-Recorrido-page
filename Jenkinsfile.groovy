0289f1a515140b0c5348981079f10ae6bcee3382
gtello79 <gonzalotello79@gmail.com>
2025-01-02 10:48:18 -0300

    add config on Jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,24 @@
 pipeline {
+    // En que agente esto estará corriendo
     agent any
+    
+    options {
+        timeout(time: 1, unit: 'HOURS') // timeout on while pipeline job
+    }
 
+    // Herramientas que se usarán en el pipeline
+    tools{
+        maven 'maven_3_9_5'
+    }
+
+    // Variables de entorno
     stages {
 
         stage('Compile Stage') {
             steps {
                 script {
                     def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    
                     echo "Maven Home: ${Nombre}"
                     withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
                         sh 'mvn -version'
@@ -37,6 +49,27 @@ pipeline {
             }
         }
 
+    }
+    post {
+        always {
+            echo 'Soy un mensaje que aparece siempre'
+        }
+
+        success {
+            echo 'Soy un mensaje que aparece si todo sale bien'
+        }
+
+        failure {
+            echo 'Soy un mensaje que aparece si algo sale mal'
+        }
+
+        cleanup {
+            echo 'Soy un mensaje que aparece al final de todo, sin importar el resultado'
+        }
+
+        regression {
+            echo 'Soy un mensaje que aparece si se ejecuta la etapa de regression, es decir, el build anterior pasó y este no'
+        }
     }
 
 }
